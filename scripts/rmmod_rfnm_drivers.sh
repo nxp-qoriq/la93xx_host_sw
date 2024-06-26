80b7b15dd2456c489a3028030c502d528d46e02c
Nikhil Gupta <nikhil.gupta_2@nxp.com>
2024-06-26 04:28:10 -0700

    la9310:Check M7 elf loaded or not before reseting
    Signed-off-by: Nikhil Gupta <nikhil.gupta_2@nxp.com>

--- scripts/rmmod_rfnm_drivers.sh
+++ scripts/rmmod_rfnm_drivers.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 #SPDX-License-Identifier: BSD-3-Clause
 #Copyright 2024 NXP
-memtool -32 0x302F0000=0x000000EA
-memtool -32 0x3039000C=000000AA
+
+m7_state=$(cat /sys/class/remoteproc/remoteproc0/state)
+if [[ "$m7_state" == "running" ]]; then
+	memtool -32 0x302F0000=0x000000EA #Stop GPT3 timer
+	memtool -32 0x3039000C=000000AA   #Reset M7 core
+fi
+
 lsmod | grep rfnm_tti
 if [[ $? -eq 0 ]]; then
 	rmmod rfnm_tti
