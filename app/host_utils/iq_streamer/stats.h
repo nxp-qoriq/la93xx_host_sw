39950fc7d9cf3c1e7b30e46f5ff2398329d0a715
Francois Massot-Pellet <francois.massot-pellet@nxp.com>
2024-10-15 13:57:09 +0200

    add bsd license
    Signed-off-by: Francois Massot-Pellet <francois.massot-pellet@nxp.com>

--- app/host_utils/iq_streamer/stats.h
+++ app/host_utils/iq_streamer/stats.h
@@ -1,18 +1,6 @@
-// =============================================================================
-//! @file       main.h
-//! @brief      main interface.
-//! @author     NXP Semiconductors.
-//! @copyright  Copyright 2023-2024 NXP
-// =============================================================================
-/*
-*  NXP Confidential. This software is owned or controlled by NXP and may only be used strictly
-*  in accordance with the applicable license terms. By expressly accepting
-*  such terms or by downloading, installing, activating and/or otherwise using
-*  the software, you are agreeing that you have read, and that you agree to
-*  comply with and are bound by, such license terms. If you do not agree to
-*  be bound by the applicable license terms, then you may not retain,
-*  install, activate or otherwise use the software.
-*/
+/* Copyright 2022-2024 NXP
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
 
 #ifndef __STATS_H__
 #define __STATS_H__
